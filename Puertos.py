b788705c7e8721693414b2a46d579c553dccb30c
Ivan-Rosales <18121532@tecmor.mx>
2020-02-27 11:58:40 -0600

    Update Puertos.py

--- Puertos.py
+++ Puertos.py
@@ -1,4 +1,11 @@
 import os
+
+hostname="www.itmorelia.edu.mx"
+respuesta=os.system("ping -c 1 "+hostname)
+if respuesta==0:
+	print(hostname+" funciona")
+else:
+	print(hostname+" no funciona")
 red = "200.33.171.0/24"
 os.system("nmap -sP "+ red)
 
